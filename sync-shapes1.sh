f0e01e0635e254f69c687a99716b8a36109a8dab
Eric Miller <eric@legoaces.org>
2016-01-22 14:48:36 -0500

    Added subtree sync script version 1

---
+++ sync-shapes1.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+git subtree pull --prefix Module\ 1/Set\ 1\:\ Curves\ and\ Surfaces/ Overleaf-shapes1 master
+git subtree push --prefix Module\ 1/Set\ 1\:\ Curves\ and\ Surfaces/ Overleaf-shapes1 master
+
