30b244b117365d95ffe41499d455ad7b415917d9
Eric Miller <eric@legoaces.org>
2016-03-30 11:42:37 -0400

    Finished smoothing and downsampling work

--- Module 2/Imports.m
+++ Module 2/Imports.m
@@ -79,3 +79,33 @@ s=o.{StandardDeviation[a]};
 b=(a-m)/s;
 c=(1/(Length@a-1))*Transpose[b].b
 ]
+
+
+(* ::Section:: *)
+(*Kernels*)
+
+
+Clear@applyKernel1;
+applyKernel1[k_,l_]:=Module[{circular, halfn},
+halfn = Floor[Length@k/2];
+circular=Sum[k[[kelem]]*RotateLeft[l,kelem-halfn-1],{kelem,Length@k}];
+(* Fix the ends *)
+circular[[1;;halfn]]=l[[1;;halfn]];
+circular[[-halfn;;-1]]=l[[-halfn;;-1]];
+Return@circular
+]
+
+
+Clear@applyKernel2;
+applyKernel2[k_,l_]:=Module[{circular, n, halfn, dim},
+halfn = Floor[Dimensions@k/2];
+n=Dimensions@k;
+dim = Dimensions@l;
+circular=Sum[k[[kelem1, kelem2]]*RotateLeft[l,{kelem1-halfn[[1]]-1,kelem2-halfn[[2]]-1}],{kelem1,n[[1]]},{kelem2,n[[2]]}];
+(* Fix the ends *)
+circular[[1;;halfn[[1]],All]]=l[[1;;halfn[[1]],All]];
+circular[[-halfn[[1]];;-1,All]]=l[[-halfn[[1]];;-1,All]];
+circular[[All,1;;halfn[[2]]]]=l[[All,1;;halfn[[2]]]];
+circular[[All,-halfn[[2]];;-1]]=l[[All,-halfn[[2]];;-1]];
+Return@circular;
+]
